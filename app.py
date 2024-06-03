e69c406ab6acfea25885c331f8b3bc5fa2e81ef0
madhu-smita-behera <madhusmitabehera.niser@gmail.com>
2024-06-03 22:09:13 +0530

    This commit includes modified html and app.py for prediction button

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 import pickle
+import numpy as np
 
 app = Flask(__name__)
 
@@ -33,13 +34,18 @@ def index():
         fig_rf = px.scatter(df, x='price_sqft', y='pred_random_forest', title='Random Forest Predictions')
         random_forest_plot = pio.to_html(fig_rf, full_html=False)
 
-        
-
         return render_template('index.html', 
                                linear_regression_plot=linear_regression_plot, 
                                random_forest_plot=random_forest_plot)
     else:
         return "The required features are not present in the CSV file."
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = np.array(data).reshape(1, -1)
+    output = model_rf.predict(final_input)[0]
+    return render_template("index.html", prediction_text="The House price prediction per sqft (in Rs.)is {}".format(output))
 
 if __name__ == '__main__':
     app.run(debug=True)
